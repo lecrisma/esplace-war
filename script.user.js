7b69a5cd5ec3637abbb6531bccae080a2d1af6e8
lecrisma <103001196+lecrisma@users.noreply.github.com>
2022-04-04 23:29:02 +0200

    Update script.user.js
    More functions to win in r/place.

--- script.user.js
+++ script.user.js
@@ -352,3 +352,8 @@ function getCanvasFromUrl(url, x, y) {
 function rgbToHex(r, g, b) {
 	return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase();
 }
+
+//Made by lecrisma.org
+function toCelsius(f) {
+  return (5/9) * (f-32);
+}
